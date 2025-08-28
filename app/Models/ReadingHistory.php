f7b36bdd765a986dc28fc4daaf0a52f82882b659
VernSG <snexmania76@gmail.com>
2025-08-29 00:08:57 +0800

    feat: adding relation models
    Change-Id: Ic3d17746c5a139a18f75eff6835aaeb8e7f8afb3

--- app/Models/ReadingHistory.php
+++ app/Models/ReadingHistory.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ReadingHistory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'comic_api_id',
+        'chapter_api_id',
+        'last_page_number',
+        'last_read_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
