54327a2b4b141b61addef4daf1365641ef5c7fbc
HyuzaNuzumi <linglegendstwo@gmail.com>
2025-08-30 15:19:53 +0100

    feat<migrations>:create_reading_history_table

---
+++ database/migrations/2025_08_28_153954_create_reading_history_table.php.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reading_history', function (Blueprint $table){
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('comic_api_id');
+            $table->string('chapter_api_id');
+            $table->integer('last_page_number');
+            $table->dateTime('last_read_at');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reading_history');
+    }
+};
